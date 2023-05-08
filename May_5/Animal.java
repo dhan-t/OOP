b9e21436f5411ad7127ae2d8b9b9eecf1da24a79
Dhan Tamparong <111488321+su1kagg@users.noreply.github.com>
2023-05-08 11:28:09 +0800

    Create Animal.java

---
+++ May_5/Animal.java
@@ -0,0 +1,28 @@
+package act4.ClassPack;
+
+import java.util.*;
+// computation = number of attendance * salary
+
+public class Animal {
+    public static Scanner sc = new Scanner(System.in);
+
+    protected String name;
+    protected String breed;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+}
