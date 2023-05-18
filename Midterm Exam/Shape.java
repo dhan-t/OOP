75e0c1e62a16cdcbc414d727f271eb249825a701
Dhan Tamparong <111488321+su1kagg@users.noreply.github.com>
2023-05-18 10:17:36 +0800

    Create Shape.java

---
+++ Midterm Exam/Shape.java
@@ -0,0 +1,39 @@
+package classPack;
+
+public class Shape {
+
+    private double param1;
+    private double param2;
+
+    public double getParam1() {
+        return param1;
+    }
+
+    public void setParam1(double param1) {
+        this.param1 = param1;
+    }
+
+    public double getParam2() {
+        return param2;
+    }
+
+    public void setParam2(double param2) {
+        this.param2 = param2;
+    }
+
+    Shape(double param1, double param2) {
+        this.param1 = param1;
+        this.param2 = param2;
+
+    }
+
+    public double getArea() {
+        return param1 * param2;
+    }
+
+    public double getVolume() {
+        return param1 * param2;
+
+    }
+
+}
