63198630bb25865ba37499a2469371f216704612
Dhan Tamparong <111488321+su1kagg@users.noreply.github.com>
2023-06-06 01:08:28 +0800

    Update InfoEncode.java

--- SGS Apk dump/java/InfoEncode.java
+++ SGS Apk dump/java/InfoEncode.java
@@ -5,107 +5,62 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.List;
+
+public class InfoView extends AppCompatActivity {
 
 
-public class InfoEncode extends AppCompatActivity {
 
-    private List<EditText> fieldChecker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_inforencoder);
+        setContentView(R.layout.activity_info_view);
 
-        EditText lname = findViewById(R.id.ptxtLName);
-        EditText fname = findViewById(R.id.ptxtFName);
-        EditText mname = findViewById(R.id.ptxtMName);
-        EditText email = findViewById(R.id.ptxtEmail);
+        Intent intent = getIntent();
 
-        EditText brtYR = findViewById(R.id.ptxtBrtYear);
-        EditText cnctN = findViewById(R.id.ptxtContact2);
+        // Retrieve the user input from the Intent extras
+        String firstName = intent.getStringExtra("firstname");
+        String middleName = intent.getStringExtra("midname");
+        String lastName = intent.getStringExtra("lastname");
+        String email = intent.getStringExtra("Email");
+        String babs = intent.getStringExtra("BABS");
+        String course = intent.getStringExtra("Course");
+        String yrLVL = intent.getStringExtra("yrlvl");
 
-        EditText bsba = findViewById(R.id.ptxtBABS);
-        EditText course = findViewById(R.id.ptxtCourse);
-        EditText yrLVL = findViewById(R.id.ptxtYrLvl);
+        String contactnum = intent.getStringExtra("contactNum");
+        String birthyear = intent.getStringExtra("birthYear");
 
-        fieldChecker = new ArrayList<>();
-        fieldChecker.add(lname);
-        fieldChecker.add(fname);
-        fieldChecker.add(mname);
-        fieldChecker.add(brtYR);
-        fieldChecker.add(cnctN);
-        fieldChecker.add(bsba);
-        fieldChecker.add(course);
-        fieldChecker.add(yrLVL);
+        TextView fullNameTextView = findViewById(R.id.txtFullName2);
+        fullNameTextView.setText(firstName + " " + middleName + " " + lastName);
+
+        TextView contactNum = findViewById(R.id.txtContact);
+        contactNum.setText(contactnum);
+
+        TextView email1 = findViewById(R.id.txtEmail);
+        email1.setText(email);
+
+        TextView fullcourse = findViewById(R.id.txtCourseInfo);
+        fullcourse.setText(babs.toUpperCase() + " " + course + " Year " + yrLVL);
 
         int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        String ageCalc = String.valueOf(currentYear - Integer.parseInt(birthyear));
 
-        Button submitBTN = findViewById(R.id.btnSBMT);
-        submitBTN.setOnClickListener(v -> {
-            boolean fieldEmpty = false;
+        TextView age = findViewById(R.id.txtAge);
+        age.setText(ageCalc + " y/o");
 
-            for (EditText field : fieldChecker) {
-                String userInput = field.getText().toString().trim();
-                if (userInput.isEmpty()) {
-                    fieldEmpty = true;
-                    break;
-                }
-            }
-            if (fieldEmpty) {
-                displayToast("Please input all fields!");
-            } else {
-
-                try {
-
-                    String lastname = lname.getText().toString().trim();
-                    String firstname = fname.getText().toString().trim();
-                    String midname = mname.getText().toString().trim();
-                    String Email = email.getText().toString().trim();
-
-                    String birthYear = brtYR.getText().toString().trim();
-                    String contactNum = cnctN.getText().toString().trim();
-
-                    String BABS = bsba.getText().toString().trim();
-                    String Course = course.getText().toString().trim();
-                    String yrlvl = yrLVL.getText().toString().trim();
-
-                    Intent intent = new Intent(InfoEncode.this, InfoView.class);
-                    intent.putExtra("firstname", firstname);
-                    intent.putExtra("midname", midname);
-                    intent.putExtra("lastname", lastname);
-                    intent.putExtra("Email", Email);
-
-                    intent.putExtra("birthYear", birthYear);
-                    intent.putExtra("contactNum", contactNum);
-
-                    intent.putExtra("BABS", BABS);
-                    intent.putExtra("Course", Course);
-                    intent.putExtra("yrlvl", yrlvl);
-
-                    startActivity(intent);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-                displayToast("Information Submitted!");
-            }
+        Button home = findViewById(R.id.btnHome);
+        home.setOnClickListener(v -> {
+            Intent i = new Intent(InfoView.this, Menu.class);
+            startActivity(i);
         });
 
-        Button btnMenu = findViewById(R.id.btnMenu);
-        btnMenu.setOnClickListener(v -> {
-            Intent i = new Intent(InfoEncode.this, Menu.class);
+        Button editInfo = findViewById(R.id.btnEditInfo);
+        editInfo.setOnClickListener(v -> {
+            Intent i = new Intent(InfoView.this, InfoEncode.class);
             startActivity(i);
         });
 
     }
-
-    private void displayToast(String message) {
-        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
-    }
-
 }
