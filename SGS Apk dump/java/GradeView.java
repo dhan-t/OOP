df571860e9bcd8e29c0795d4071afa3888af4706
Dhan Tamparong <111488321+su1kagg@users.noreply.github.com>
2023-06-06 01:07:55 +0800

    Update GradeView.java

--- SGS Apk dump/java/GradeView.java
+++ SGS Apk dump/java/GradeView.java
@@ -1,65 +1,104 @@
 package com.example.june5_oop;
 
-import com.example.june5_oop.ui.userInputRetainer;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class GradeView extends AppCompatActivity {
-
-    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grade_view);
+        Intent intent = getIntent();
+        String firstName = intent.getStringExtra("firstname");
+        String middleName = intent.getStringExtra("midname");
+        String lastName = intent.getStringExtra("lastname");
 
-        userInputRetainer inputRetainer = userInputRetainer.getInstance();
+        int attendance = intent.getIntExtra("attendance", 0);
+        int exams = intent.getIntExtra("exams", 0);
+        int quiz1 = intent.getIntExtra("quiz1", 0);
+        int quiz2 = intent.getIntExtra("quiz2", 0);
+        int quiz3 = intent.getIntExtra("quiz3", 0);
+        int quiz4 = intent.getIntExtra("quiz4", 0);
 
         TextView FullnameGV = findViewById(R.id.txtFullNameGV);
-        TextView CourseGV = findViewById(R.id.txtCourseGV);
+        FullnameGV.setText(firstName + " " + middleName + " " + lastName);
+
+        double convertedAve = average(attendance, exams, quiz1, quiz2, quiz3, quiz4);
         TextView txtAverageGV = findViewById(R.id.txtAverageGV);
+        txtAverageGV.setText("Average: " + String.valueOf(convertedAve));
+
         TextView txtStatusGV = findViewById(R.id.txtStatusGV);
+        txtStatusGV.setText("Status: " + passOrFail(convertedAve));
+
         TextView txtExamGV = findViewById(R.id.txtExamGV);
+        txtExamGV.setText("Exam: " + String.valueOf(exams));
+
         TextView txtQz1GV = findViewById(R.id.txtQz1GV);
+        txtQz1GV.setText("Quiz 1: " + String.valueOf(quiz1));
+
         TextView txtQz2GV = findViewById(R.id.txtQz2GV);
+        txtQz2GV.setText("Quiz 2: " + String.valueOf(quiz2));
+
         TextView txtQz3GV = findViewById(R.id.txtQz3GV);
+        txtQz3GV.setText("Quiz 3: " + String.valueOf(quiz3));
+
         TextView txtQz4GV = findViewById(R.id.txtQz4GV);
+        txtQz4GV.setText("Quiz 4: " + String.valueOf(quiz4));
+
         TextView txtAttendanceGV = findViewById(R.id.txtAttendanceGV);
+        txtAttendanceGV.setText("Attendance: " + String.valueOf(attendance));
+
         TextView txtRemarks = findViewById(R.id.txtRemarks);
-
-        String fullname = String.valueOf(inputRetainer.fullNameUIR());
-        String fullcourse = String.valueOf(inputRetainer.fullCourseUIR());
-        String average = String.valueOf(inputRetainer.aveCompute());
-        String exams = String.valueOf(inputRetainer.examOnly());
-        String qz1 = String.valueOf(inputRetainer.qz1Only());
-        String qz2 = String.valueOf(inputRetainer.qz2Only());
-        String qz3 = String.valueOf(inputRetainer.qz3Only());
-        String qz4 = String.valueOf(inputRetainer.qz4Only());
-        String attendance = String.valueOf(inputRetainer.attendanceOnly());
-        String remarks = String.valueOf(inputRetainer.remarks());
-        String passOrFail = String.valueOf(inputRetainer.passOrFail());
-
-        txtStatusGV.setText("Status: " + passOrFail);
-        txtRemarks.setText("Remarks: " + remarks);
-        FullnameGV.setText(fullname);
-        CourseGV.setText(fullcourse);
-        txtAverageGV.setText("Average: " + average);
-        txtExamGV.setText("Exams: " + exams);
-        txtQz1GV.setText("Quiz 1: " + qz1);
-        txtQz2GV.setText("Quiz 2: " + qz2);
-        txtQz3GV.setText("Quiz 3: " + qz3);
-        txtQz4GV.setText("Quiz 4: " + qz4);
-        txtAttendanceGV.setText("Attendance: " + attendance);
+        txtRemarks.setText("Remarks: " + remarksResult(convertedAve));
 
         Button btnMenu = findViewById(R.id.btnMenuGV);
         btnMenu.setOnClickListener(v -> {
             Intent i = new Intent(GradeView.this, Menu.class);
             startActivity(i);
         });
+    }
+
+    private double average(int attendance, int exams, int quiz1, int quiz2, int quiz3, int quiz4) {
+        int quizAverage = ((quiz1) + (quiz2) + (quiz3) + (quiz4)) / 4;
+        double averageCalc = ((attendance) * 0.2) + (quizAverage * 0.3) + ((exams) * 0.5);
+
+        return averageCalc;
+    }
+
+    private String remarksResult (double averagerr){
+        String remarks;
+    if (averagerr >= 96 && averagerr <= 100) {
+            remarks = "4.00";
+        } else if (averagerr >= 90 && averagerr <= 95) {
+            remarks = "3.50";
+        } else if (averagerr >= 84 && averagerr <= 89) {
+            remarks = "3.00";
+        } else if (averagerr >= 78 && averagerr <= 83) {
+            remarks = "2.50";
+        } else if (averagerr >= 72 && averagerr <= 77) {
+            remarks = "2.00";
+        } else if (averagerr >= 66 && averagerr <= 71) {
+            remarks = "1.50";
+        } else if (averagerr >= 60 && averagerr <= 65) {
+            remarks = "1.00";
+        } else {
+            remarks = "INC";
+        }
+        return remarks;
+    }
+
+    private String passOrFail(double averagepof){
+        String passorfail;
+
+        if (averagepof >= 60) {
+            passorfail = "Passed";
+        } else {
+            passorfail = "Fail";
+        }
+        return passorfail;
     }
 }
