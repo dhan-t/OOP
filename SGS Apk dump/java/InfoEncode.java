c33d60b5d1fc63617a46c7449a6af5866f685f47
Dhan Tamparong <111488321+su1kagg@users.noreply.github.com>
2023-06-06 01:08:15 +0800

    Update InfoEncode.java

--- SGS Apk dump/java/InfoEncode.java
+++ SGS Apk dump/java/InfoEncode.java
@@ -1,26 +1,21 @@
 package com.example.june5_oop;
 
-import com.example.june5_oop.ui.userInputRetainer;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.InputFilter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
-import kotlinx.coroutines.debug.internal.StackTraceFrame;
 
 public class InfoEncode extends AppCompatActivity {
 
     private List<EditText> fieldChecker;
-    private userInputRetainer inputRetainer;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +24,7 @@ public class InfoEncode extends AppCompatActivity {
         EditText lname = findViewById(R.id.ptxtLName);
         EditText fname = findViewById(R.id.ptxtFName);
         EditText mname = findViewById(R.id.ptxtMName);
+        EditText email = findViewById(R.id.ptxtEmail);
 
         EditText brtYR = findViewById(R.id.ptxtBrtYear);
         EditText cnctN = findViewById(R.id.ptxtContact2);
@@ -37,33 +33,17 @@ public class InfoEncode extends AppCompatActivity {
         EditText course = findViewById(R.id.ptxtCourse);
         EditText yrLVL = findViewById(R.id.ptxtYrLvl);
 
-
-        InputFilter[] filters = new InputFilter[1];
-        filters[0] = new InputFilter.LengthFilter(15); // Set the desired character limit here
-
-        cnctN.setFilters(filters);
-
         fieldChecker = new ArrayList<>();
         fieldChecker.add(lname);
         fieldChecker.add(fname);
         fieldChecker.add(mname);
-
         fieldChecker.add(brtYR);
         fieldChecker.add(cnctN);
-
         fieldChecker.add(bsba);
         fieldChecker.add(course);
         fieldChecker.add(yrLVL);
 
-        inputRetainer = userInputRetainer.getInstance();
-
-
-        String input = cnctN.getText().toString();
-        try {
-            long number = Long.parseLong(input);
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-        }
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
 
         Button submitBTN = findViewById(R.id.btnSBMT);
         submitBTN.setOnClickListener(v -> {
@@ -81,19 +61,37 @@ public class InfoEncode extends AppCompatActivity {
             } else {
 
                 try {
-                    inputRetainer.fullNameCons(fname.getText().toString().trim(), mname.getText().toString().trim(), lname.getText().toString().trim());
-                    inputRetainer.getContactNum();
-                    inputRetainer.birthYear();
-                    inputRetainer.fullCourseCons(bsba.getText().toString().trim(), course.getText().toString().trim(), Integer.parseInt(yrLVL.getText().toString().trim()));
 
+                    String lastname = lname.getText().toString().trim();
+                    String firstname = fname.getText().toString().trim();
+                    String midname = mname.getText().toString().trim();
+                    String Email = email.getText().toString().trim();
 
-                    UserSession.loggedIn = true;
+                    String birthYear = brtYR.getText().toString().trim();
+                    String contactNum = cnctN.getText().toString().trim();
 
-                    Intent i = new Intent(InfoEncode.this, InfoView.class);
-                    startActivity(i);
-                }catch (Exception e){
+                    String BABS = bsba.getText().toString().trim();
+                    String Course = course.getText().toString().trim();
+                    String yrlvl = yrLVL.getText().toString().trim();
+
+                    Intent intent = new Intent(InfoEncode.this, InfoView.class);
+                    intent.putExtra("firstname", firstname);
+                    intent.putExtra("midname", midname);
+                    intent.putExtra("lastname", lastname);
+                    intent.putExtra("Email", Email);
+
+                    intent.putExtra("birthYear", birthYear);
+                    intent.putExtra("contactNum", contactNum);
+
+                    intent.putExtra("BABS", BABS);
+                    intent.putExtra("Course", Course);
+                    intent.putExtra("yrlvl", yrlvl);
+
+                    startActivity(intent);
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
+
                 displayToast("Information Submitted!");
             }
         });
@@ -110,7 +108,4 @@ public class InfoEncode extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 
-    public static class UserSession {
-        public static boolean loggedIn = false;
-    }
 }
